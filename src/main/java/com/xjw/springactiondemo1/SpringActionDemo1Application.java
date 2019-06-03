4971ba142f8bbffbb73eabce7a547ec95487bb18
ChickenRiceWithEgg <jingwei144@foxmail.com>
2019-06-03 21:49:28 +0800

    使用@Component和@ComponentScan创建可被发现的bean，使用@Autowired装备容器中的bean，使用@Configuration声明一个配置类，使用@ContextConfiguration来加载配置类，使用@SpringJUnit 4ClassRunner在测试的时候自动创建Spring的应用上下文

---
+++ src/main/java/com/xjw/springactiondemo1/SpringActionDemo1Application.java
@@ -0,0 +1,13 @@
+package com.xjw.springactiondemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActionDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringActionDemo1Application.class, args);
+    }
+
+}
